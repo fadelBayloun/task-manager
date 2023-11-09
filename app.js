adc77ef0b11bca3f98a297da82b87de749704dc2
fadelBayloun <fadel.bayloun12@gmail.com>
2023-11-09 18:35:47 +0200

    added the delete functionality

--- app.js
+++ app.js
@@ -14,6 +14,31 @@ addButton.addEventListener("click", function () {
       id: tasks.length,
     };
     tasks.push(task);
-    console.log(tasks);
+    taskList.innerHTML += `<li>${task.name}
+    <button id="delete-task-button" class="${task.id}">Delete</button></li>`;
+
+    // Deleting the task from Html and From Tasks Array
+    let deleteButton = document.querySelector("." + task.id.toString());
+    deleteButton.addEventListener("click", function (e) {
+      let parent = e.target.parentElement;
+      parent.remove();
+    });
   }
 });
+
+//   let taskid = Number(e.target.className);
+//   console.log(parent);
+//   tasks.splice(taskid, 1);
+
+// Editing the task in Html and in Tasks Array
+// let editButton = document.getElementById("edit-task-button");
+// editButton.addEventListener("click", function (e) {
+//   parent = e.target.parentElement;
+//   taskid = Number(e.target.className);
+//   newTask = prompt("Enter your new value");
+//   tasks[taskid].name = newTask;
+//   parent.innerHTML += `<li>${newTask}<button id="edit-task-button" class="${task.id}">edit</button>
+// <button id="delete-task-button" class="${task.id}">Delete</button></li>`;
+// });
+
+// <button id="edit-task-button" class="${task.id}">edit</button>
