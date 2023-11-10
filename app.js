726d3161c1d77ed5d8e8106b94a6c23045220dec
fadelBayloun <fadel.bayloun12@gmail.com>
2023-11-10 08:26:45 +0200

    finished the edit button and fixed delete button

--- app.js
+++ app.js
@@ -1,44 +1,31 @@
-let addButton = document.getElementById("add-task-button");
-let taskInput = document.getElementById("task-input");
-let taskList = document.getElementById("task-list");
-let tasks = [];
+let list = document.getElementsByTagName("ul")[0];
+let addButton = document.getElementById("addButton");
+let input = document.getElementById("task");
 
-// Adding new task
-addButton.addEventListener("click", function () {
-  let taskName = taskInput.value.trim();
-
-  //   validation for empty task name
-  if (taskName !== "") {
-    let task = {
-      name: taskName,
-      id: tasks.length,
-    };
-    tasks.push(task);
-    taskList.innerHTML += `<li>${task.name}
-    <button id="delete-task-button" class="${task.id}">Delete</button></li>`;
-
-    // Deleting the task from Html and From Tasks Array
-    let deleteButton = document.querySelector("." + task.id.toString());
-    deleteButton.addEventListener("click", function (e) {
-      let parent = e.target.parentElement;
-      parent.remove();
+addButton.addEventListener("click", function (e) {
+  if (input.value !== "") {
+    let li = document.createElement("li");
+    let deleteButton = document.createElement("button");
+    let editButton = document.createElement("button");
+    deleteButton.innerText = "delete";
+    editButton.innerText = "edit";
+    li.innerText = input.value;
+    input.value = "";
+    li.appendChild(deleteButton);
+    li.appendChild(editButton);
+    editButton.addEventListener("click", function (e) {
+      let target = e.target.parentElement;
+      target.innerText = prompt();
+      target.appendChild(deleteButton);
+      target.appendChild(editButton);
     });
+    deleteButton.onclick = (e) => {
+      e.target.parentElement.remove();
+    };
+    list.appendChild(li);
+
+    // add the li to the local storage or array thing
+
+    document.body.appendChild(li);
   }
 });
-
-//   let taskid = Number(e.target.className);
-//   console.log(parent);
-//   tasks.splice(taskid, 1);
-
-// Editing the task in Html and in Tasks Array
-// let editButton = document.getElementById("edit-task-button");
-// editButton.addEventListener("click", function (e) {
-//   parent = e.target.parentElement;
-//   taskid = Number(e.target.className);
-//   newTask = prompt("Enter your new value");
-//   tasks[taskid].name = newTask;
-//   parent.innerHTML += `<li>${newTask}<button id="edit-task-button" class="${task.id}">edit</button>
-// <button id="delete-task-button" class="${task.id}">Delete</button></li>`;
-// });
-
-// <button id="edit-task-button" class="${task.id}">edit</button>
