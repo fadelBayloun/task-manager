2bbf447c54c2506906537298f26c4966425b39f8
fadelBayloun <fadel.bayloun12@gmail.com>
2023-11-10 18:15:23 +0200

    added some design

--- app.js
+++ app.js
@@ -5,27 +5,46 @@ let input = document.getElementById("task");
 addButton.addEventListener("click", function (e) {
   if (input.value !== "") {
     let li = document.createElement("li");
-    let deleteButton = document.createElement("button");
-    let editButton = document.createElement("button");
-    deleteButton.innerText = "delete";
-    editButton.innerText = "edit";
     li.innerText = input.value;
     input.value = "";
+
+    let deleteIcon = document.createElement("i");
+    deleteIcon.innerText = "delete";
+    deleteIcon.classList.add("material-icons");
+    let deleteButton = document.createElement("button");
+    deleteButton.classList.add("deleteButton");
+    deleteButton.appendChild(deleteIcon);
     li.appendChild(deleteButton);
+
+    let editIcon = document.createElement("i");
+    editIcon.innerText = "edit";
+    editIcon.classList.add("material-icons");
+    let editButton = document.createElement("button");
+    editButton.classList.add("editButton");
+    editButton.appendChild(editIcon);
     li.appendChild(editButton);
+
     editButton.addEventListener("click", function (e) {
       let target = e.target.parentElement;
-      target.innerText = prompt("Enter your edited task");
-      target.appendChild(deleteButton);
-      target.appendChild(editButton);
+      let newText = prompt("Enter your edited task");
+      if (newText !== "") {
+        target.innerText = newText;
+        target.appendChild(deleteButton);
+        target.appendChild(editButton);
+      }
     });
+
     deleteButton.addEventListener("click", function (e) {
-      e.target.parentElement.remove();
+      console.log(e.target.parentElement);
+      if (e.target.parentElement.classList.contains("deleteButton")) {
+        e.target.parentElement.parentElement.remove();
+      } else {
+        e.target.parentElement.remove();
+      }
     });
+
     list.appendChild(li);
-
     // add the li to the local storage or array thing
-
     document.body.appendChild(li);
   }
 });
